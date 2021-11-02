6cb654f490b7423260ea870a867e0fb9f5d41b5c
joeldevel <christianjoel.developer@gmail.com>
2021-11-02 17:17:44 -0400

    Crea pantalla bienvenida con animacion

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, StatusBar, Image, Animated } from 'react-native';
+import Rocket from './assets/rocket.png';
 
-export default function App() {
+const App = () => {
+  const [show] = useState(new Animated.Value(0));
+  const [position] = useState(new Animated.Value(700));
+
+  useEffect(()=> {
+    Animated.parallel([
+      Animated.timing( show, {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: false,
+        delay: 1000
+      }),
+      Animated.timing( position, {
+        toValue: -700,
+        duration: 3000,
+        useNativeDriver: false,
+      })
+    ]).start();
+  }, []);
+
   return (
+    <>
+    <StatusBar animated={true} backgroundColor="#142950" barStyle="#light-content">
+    </StatusBar>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.Image style={[styles.image, {top: position}]} source={Rocket} />
+      <Animated.Text style={[styles.text, {opacity: show}]}>Bienvenido</Animated.Text>
     </View>
+    </>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#142950',
+    justifyContent: 'space-around'
   },
+  text: {
+    color: '#fafafa',
+    fontSize: 50
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain'
+  }
 });
+export default App;
